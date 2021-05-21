a4168a484dc38cac89ce2719e819b3773ec591ae
Olugbenga Ajayi <gbejula@gmail.com>
2021-05-21 14:17:35 +0100

    added functionality for close using modal and backdrop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from './components/Cart/Cart';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <React.Fragment>
-      <Header />
-      <Cart />
+      <Header onShowCart={showCartHandler} />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <main>
         <Meals />
       </main>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,13 +4,15 @@ import ReactDOM from 'react-dom';
 import classes from './Modal.module.css';
 
 const BackDrop = props => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 
 const ModalOverlays = props => {
   return (
     <div className={classes.modal}>
-      <div className={classes.content}>{props.children}</div>
+      <div className={classes.content} onClick={props.onDismiss}>
+        {props.children}
+      </div>
     </div>
   );
 };
@@ -20,9 +22,14 @@ const portalElements = document.getElementById('overlays');
 const Modal = props => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<BackDrop />, portalElements)}
       {ReactDOM.createPortal(
-        <ModalOverlays>{props.children}</ModalOverlays>,
+        <BackDrop onClose={props.onClose} />,
+        portalElements
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlays onDismiss={props.onDismiss}>
+          {props.children}
+        </ModalOverlays>,
         portalElements
       )}
     </Fragment>
