39f34475d923c1abb5cadfc47bed83fbccc8d38f
Olugbenga Ajayi <gbejula@gmail.com>
2021-06-21 23:25:50 +0100

    added state for checkout

--- src/components/Cart/CheckOut.js
+++ src/components/Cart/CheckOut.js
@@ -1,27 +1,39 @@
+import { useRef } from 'react-dom';
+
 import classes from './CheckOut.module.css';
 
 const CheckOut = props => {
+  const nameInputRef = useRef();
+  const addressInputRef = useRef();
+  const stateInputRef = useRef();
+  const cityInputRef = useRef();
+
   const confirmHandler = event => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+    const enteredState = stateInputRef.current.value;
   };
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor='name'>Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' ref={nameInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor='address'>Address</label>
-        <input type='text' id='address' />
+        <input type='text' id='address' ref={addressInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor='city'>City</label>
-        <input type='text' id='city' />
+        <input type='text' id='city' ref={cityInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor='state'>State</label>
-        <input type='text' id='state' />
+        <input type='text' id='state' ref={stateInputRef} />
       </div>
       <div className={classes.actions}>
         <button type='button' onClick={props.onCancel}>
